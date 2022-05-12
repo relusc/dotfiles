86bcaac3ab3f4e70730114a4eee4a8f197021f1a
Rene Schach <r.schach96@gmail.com>
2022-05-12 12:59:16 +0200

    Initial (#1)
    * Add first files
    
    * Update settings.json

---
+++ vscode/install-extensions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Small script that installs VSCode extensions exported to a file (in this case the file is named "extensions")
+# List VSCode extensions: 'code --list-extensions'
+
+set -euo pipefail
+
+while read -r line; do code --install-extension "$line"; done < extensions
